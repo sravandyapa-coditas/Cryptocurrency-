f2fe4d7d7b354b0c677cec56cc878487ce79bfd0
sravan dyapa <dyapa.reddy@coditas.com>
2023-07-27 12:05:41 +0530

    Feat: First Commit

---
+++ src/main/java/com/example/cryptocurrency/CryptoCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyApplication.class, args);
+    }
+
+}
